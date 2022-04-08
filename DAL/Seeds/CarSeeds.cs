0f10ee66cbf62aa931cec52c2b3ea22a229f9e89
Kryštof Albrecht <xalbre05@vutbr.cz>
2022-04-08 15:54:55 +0200

    Added user Guids to Car seeds.

--- DAL/Seeds/CarSeeds.cs
+++ DAL/Seeds/CarSeeds.cs
@@ -17,10 +17,10 @@ public static class CarSeeds
         Manufacturer: "Skoda",
         Type: CarType.Sedan,
         LicensePlate: "DIKTAT0R",
-        RegistrationDate: new DateTime(year: 2000, month: 03, day:15),
+        RegistrationDate: new DateTime(year: 2000, month: 03, day: 15),
         SeatCount: 4,
         PhotoUrl: @"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.klassiekerweb.nl%2Fwp-content%2Fuploads%2F2014%2F04%2Fskoda_octavia_1959.jpg&f=1&nofb=1",
-        CarOwnerId: Guid.NewGuid()); //TODO add user id from userseeds
+        CarOwnerId: Guid.Parse("6860fad0-cd02-47b7-af5d-194288d2947b"));
 
     public static readonly CarEntity Mercedes = new(
 
@@ -31,7 +31,7 @@ public static class CarSeeds
         RegistrationDate: new DateTime(year: 2015, month: 12, day: 01),
         SeatCount: 4,
         PhotoUrl: @"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.autocar.co.uk%2Fsites%2Fautocar.co.uk%2Ffiles%2Fimages%2Fcar-reviews%2Ffirst-drives%2Flegacy%2F1-mercedes-cla-shooting-brake-220d-2020-uk-fd-hero-front.jpg&f=1&nofb=1",
-        CarOwnerId: Guid.NewGuid()); //TODO add user id from userseeds
+        CarOwnerId: Guid.Parse("6860fad0-cd02-47b7-af5d-194288d2947b"));
 
 
     public static void Seed(this ModelBuilder modelBuilder)

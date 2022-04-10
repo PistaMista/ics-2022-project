21166931981a0a7e775361ab655e0e0bd412ee6d
xmarek75 <xmarek75@vutbr.cz>
2022-04-10 23:09:20 +0200

    add ridefacadetests

--- CarPool.BL.Tests/RideFacadeTests.cs
+++ CarPool.BL.Tests/RideFacadeTests.cs
@@ -39,5 +39,64 @@ namespace CarPool.BL.Tests
 
             var _ = await _rideFacadeSut.SaveAsync(model);
         }
+
+        [Fact]
+        public async Task GetAll_Single_SeededRide()
+        {
+            var rides = await _rideFacadeSut.GetAsync();
+            var ride = rides.Single(i => i.Id == RideSeeds.RideEntity.Id);
+
+            DeepAssert.Equal(Mapper.Map<RideInfoModel>(RideSeeds.RideEntity), ride);
+        }
+        [Fact]
+        public async Task GetById_SeededRide()
+        {
+            var ride = await _rideFacadeSut.GetAsync(RideSeeds.RideEntity.Id);
+
+            DeepAssert.Equal(Mapper.Map<RideModel>(RideSeeds.RideEntity), ride);
+        }
+        [Fact]
+        public async Task GetById_NonExistent()
+        {
+            var ride = await _rideFacadeSut.GetAsync(Guid.Empty);
+
+            Assert.Null(ride);
+        }
+        [Fact]
+        public async Task SeededRide_DeleteById_Deleted()
+        {
+            await _rideFacadeSut.DeleteAsync(RideSeeds.RideEntity.Id);
+
+            await using var dbxAssert = await DbContextFactory.CreateDbContextAsync();
+            Assert.False(await dbxAssert.Rides.AnyAsync(i => i.Id == RideSeeds.RideEntity.Id));
+        }
+        [Fact]
+        public async Task SeededRide_InsertOrUpdate_RideUpdated()
+        {
+            //Arrange
+            var ride = new RideModel
+            (
+                StartLocation: @"Praha",
+                EndLocation: @"Brno",
+                StartTime: new DateTime(2022, 12, 12),
+                Duration: 150,
+                CarId: CarSeeds.CarEntity.Id,
+                DriverId: UserSeeds.UserEntity.Id
+            )
+            {
+                Id = RideSeeds.RideEntity.Id
+            };
+
+            ride.EndLocation = "Varsava";
+            ride.StartTime = new DateTime(year: 2023, month: 12, day: 01);
+
+            //Act
+            await _rideFacadeSut.SaveAsync(ride);
+
+            //Assert
+            await using var dbxAssert = await DbContextFactory.CreateDbContextAsync();
+            var rideFromDb = await dbxAssert.Rides.SingleAsync(i => i.Id == ride.Id);
+            DeepAssert.Equal(ride, Mapper.Map<RideModel>(rideFromDb));
+        }
     }
 }

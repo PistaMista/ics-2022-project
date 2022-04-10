5d44e75d33d66426bb3d2e9d361cc3d05b79980b
Kryštof Albrecht <xalbre05@vutbr.cz>
2022-04-10 16:00:07 +0200

    Added a few more tests.

--- DAL.Tests/DbContextUserTests.cs
+++ DAL.Tests/DbContextUserTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using DAL.Seeds;
+using DAL.Entities;
 using Common.Tests;
 
 using Xunit;
@@ -17,21 +18,24 @@ public class DbContextUserTests : DbContextTestsBase
 {
     public DbContextUserTests(ITestOutputHelper output) : base(output)
     {
-
+       
     }
 
     [Fact]
     public async Task AddNew_UserWithoutCarsOrRides_Persisted()
     {
-        var lubomir = UserSeeds.Lubomir;
+        var marie = UserSeeds.Marie with
+        {
+            Id = Guid.NewGuid()
+        };
 
-        CarRideDbContextSUT.Add(lubomir);
+        CarRideDbContextSUT.Add(marie);
         await CarRideDbContextSUT.SaveChangesAsync();
 
         await using var dbx = await DbContextFactory.CreateDbContextAsync();
-        var actualLubomir = await dbx.Users.SingleAsync(u => u.Id == lubomir.Id);
+        var actualMarie = await dbx.Users.SingleAsync(u => u.Id == marie.Id);
 
-        DeepAssert.Equal(lubomir, actualLubomir);
+        DeepAssert.Equal(marie, actualMarie);
     }
 
     [Fact]
@@ -39,10 +43,15 @@ public class DbContextUserTests : DbContextTestsBase
     {
         var jane = UserSeeds.Marie with
         {
-            FirstName = "Jane"
+            Id = Guid.NewGuid(),
+            FirstName = "Jane",
+            Cars = new List<CarEntity>()
         };
 
-        var skoda = CarSeeds.Skoda;
+        var skoda = CarSeeds.Skoda with
+        {
+            Id = Guid.NewGuid()
+        };
         jane.Cars.Add(skoda);
 
         CarRideDbContextSUT.Add(jane);
@@ -54,5 +63,40 @@ public class DbContextUserTests : DbContextTestsBase
             .SingleAsync(u => u.Id == jane.Id);
 
         DeepAssert.Equal(jane, actualJane);
+    }
+
+    [Fact]
+    public async Task AddNew_Driver_Persisted()
+    {
+        var lubomir = UserSeeds.Lubomir with
+        {
+            Id = Guid.NewGuid(),
+            Cars = new List<CarEntity>(),
+            RidesDriver = new List<RideEntity>()
+        };
+
+        var mercedes = CarSeeds.Mercedes with
+        {
+            Id = Guid.NewGuid()
+        };
+
+        var praha_brno = RideSeeds.praha_brno with
+        {
+            Id = Guid.NewGuid()
+        };
+
+        lubomir.Cars.Add(mercedes);
+        lubomir.RidesDriver.Add(praha_brno);
+
+        CarRideDbContextSUT.Add(lubomir);
+        await CarRideDbContextSUT.SaveChangesAsync();
+
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        var actualLubomir = await dbx.Users
+            .Include(u => u.Cars)
+            .Include(u => u.RidesDriver)
+            .SingleAsync(u => u.Id == lubomir.Id);
+
+        DeepAssert.Equal(lubomir, actualLubomir);
     }
 }

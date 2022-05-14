3a23cbc1c400636fd1238fe07b6b414dcf21acc6
Kryštof Albrecht <xalbre05@vutbr.cz>
2022-05-14 12:46:29 +0200

    Added CarInfoViewModel for testing.

---
+++ CarPool.App/ViewModels/CarInfoViewModel.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CarPool.App.Commands;
+using CarPool.App.Messages;
+using CarPool.App.Services;
+using CarPool.App.Services.MessageDialog;
+using CarPool.App.Wrappers;
+using CarPool.BL.Facades;
+using CarPool.BL.Models;
+
+namespace CarPool.App.ViewModels
+{
+    public class CarInfoViewModel : ViewModelBase
+    {
+        private readonly IMediator _mediator;
+        private readonly CarFacade _carFacade;
+        private readonly IMessageDialogService _messageDialogService;
+
+        public CarInfoViewModel(
+            CarFacade carFacade,
+            IMessageDialogService messageDialogService,
+            IMediator mediator)
+        {
+            _carFacade = carFacade;
+            _messageDialogService = messageDialogService;
+            _mediator = mediator;
+
+            SaveCommand = new AsyncRelayCommand(SaveAsync, CanSave);
+            DeleteCommand = new AsyncRelayCommand(DeleteAsync);
+        }
+
+        public CarWrapper? Model { get; private set; }
+        public ICommand SaveCommand { get; }
+        public ICommand DeleteCommand { get; }
+
+
+        public async Task LoadAsync(Guid id)
+        {
+            Model = await _carFacade.GetAsync(id) ?? CarModel.Empty;
+        }
+
+        public async Task SaveAsync()
+        {
+            if (Model == null)
+            {
+                throw new InvalidOperationException("Null model cannot be saved");
+            }
+
+            Model = await _carFacade.SaveAsync(Model.Model);
+            _mediator.Send(new UpdateMessage<CarWrapper> { Model = Model });
+        }
+
+        private bool CanSave() => Model?.IsValid ?? false;
+
+        public async Task DeleteAsync()
+        {
+            if (Model is null)
+            {
+                throw new InvalidOperationException("Null model cannot be deleted");
+            }
+
+            if (Model.Id != Guid.Empty)
+            {
+                var delete = _messageDialogService.Show(
+                    $"Delete",
+                    $"Do you want to delete {Model?.Manufacturer} {Model?.Type}?.",
+                    MessageDialogButtonConfiguration.YesNo,
+                    MessageDialogResult.No);
+
+                if (delete == MessageDialogResult.No) return;
+
+                try
+                {
+                    await _carFacade.DeleteAsync(Model!.Id);
+                }
+                catch
+                {
+                    var _ = _messageDialogService.Show(
+                        $"Deleting of {Model?.Manufacturer} {Model?.Type} failed!",
+                        "Deleting failed",
+                        MessageDialogButtonConfiguration.OK,
+                        MessageDialogResult.OK);
+                }
+
+                _mediator.Send(new DeleteMessage<CarWrapper>
+                {
+                    Model = Model
+                });
+            }
+        }
+
+        //public override void LoadInDesignMode()
+        //{
+        //    base.LoadInDesignMode();
+        //}
+    }
+}

25c246a97723630f25109278adedcdc3834fa7b5
Kryštof Albrecht <xalbre05@vutbr.cz>
2022-05-15 17:15:22 +0200

    Added editing of user name.

--- CarPool.App/ViewModels/ManageAccountViewModel.cs
+++ CarPool.App/ViewModels/ManageAccountViewModel.cs
@@ -27,11 +27,15 @@ namespace CarPool.App.ViewModels
             _messageDialogService = messageDialogService;
             _mediator = mediator;
 
+            SaveCommand = new AsyncRelayCommand(SaveAsync, CanSave);
 
             _mediator.Register<SelectedMessage<UserWrapper>>(async x => {
                 await LoadAsync(x.Id ?? Guid.Empty);
             });
         }
+
+        public ICommand SaveCommand { get;  }
+
 
         private UserWrapper? _model;
         public UserWrapper? Model { get => _model; private set
@@ -45,5 +49,17 @@ namespace CarPool.App.ViewModels
         {
             Model = await _userFacade.GetAsync(id) ?? UserModel.Empty;
         }
+
+        public async Task SaveAsync()
+        {
+            if (Model == null)
+            {
+                throw new InvalidOperationException("Null model cannot be saved");
+            }
+
+            Model = await _userFacade.SaveAsync(Model.Model);
+            _mediator.Send(new UpdateMessage<UserWrapper> { Model = Model });
+        }
+        private bool CanSave() => Model?.IsValid ?? false;
     }
 }

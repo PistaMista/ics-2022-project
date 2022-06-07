e75c6398ab423018d0ff7eafe819f5e67090846f
Kryštof Albrecht <xalbre05@vutbr.cz>
2022-06-07 16:19:44 +0200

    Added constructor to UserEntity (for collection mapping).

--- CarPool.DAL/Entities/UserEntity.cs
+++ CarPool.DAL/Entities/UserEntity.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace CarPool.DAL.Entities;
@@ -8,6 +8,8 @@ public record UserEntity (
     string? LastName,
     string? PhotoUrl) : IEntity
 {
+    public UserEntity() : this(default, default, default, default) { }
+
     public ICollection<CarEntity> Cars { get; init; } = new List<CarEntity>();
     public ICollection<RideEntity> RidesDriver { get; init; } = new List<RideEntity>();
     public ICollection<RideEntity> RidesPassenger { get; init; } = new List<RideEntity>();
